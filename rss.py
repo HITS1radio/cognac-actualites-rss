daf80863d7db7b5637b4fc80007be5d8a1db9be2
HITS1radio <contact@hits1radio.com>
2026-08-02 14:23:45 +0200

    Implement RSS feed generation for Cognac news
    This function generates an RSS feed for Cognac news articles, including titles, links, descriptions, and publication dates.

--- rss.py
+++ rss.py
@@ -1,1 +1,76 @@
+from feedgen.feed import FeedGenerator
+import html
 
+from utils import date_rss
+
+
+def creer_flux(articles):
+
+    fg = FeedGenerator()
+
+
+    fg.title(
+        "Actualités Cognac"
+    )
+
+
+    fg.link(
+        href="https://hits1radio.github.io/cognac-actualites-rss/rss.xml"
+    )
+
+
+    fg.description(
+        "Actualités locales de Cognac - Charente Libre et Sud Ouest"
+    )
+
+
+    for article in articles[:100]:
+
+        item = fg.add_entry()
+
+
+        item.title(
+            html.escape(
+                article["title"]
+            )
+        )
+
+
+        item.link(
+            href=article["url"]
+        )
+
+
+        description = f"""
+        <p>
+        <strong>{html.escape(article['title'])}</strong>
+        </p>
+
+        <p>
+        📰 {html.escape(article['source'])}
+        </p>
+
+        <p>
+        {html.escape(article['description'])}
+        </p>
+        """
+
+
+        item.description(
+            description
+        )
+
+
+        item.pubDate(
+            date_rss()
+        )
+
+
+        item.guid(
+            article["url"]
+        )
+
+
+    fg.rss_file(
+        "rss.xml"
+    )
